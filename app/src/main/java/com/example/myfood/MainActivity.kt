19fbf20f700b62ab2fea0beeed6386ece47baaad
Anderson Roberto de Oliveira <andersonrobertobru@gmail.com>
2022-09-01 20:15:32 -0300

    Initial commit

---
+++ app/src/main/java/com/example/myfood/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.myfood
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.myfood.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+        val navController: NavController = findNavController(R.id.nav_host_fragment_activity_main)
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_search, R.id.navigation_requests
+            )
+        )
+        //setupActionBarWithNavController(navController!!, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
