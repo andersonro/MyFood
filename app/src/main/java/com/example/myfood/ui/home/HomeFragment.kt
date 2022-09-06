aa14077e59dbfc782d7e2fe05d1a102e5da574dc
Anderson Roberto de Oliveira <andersonrobertobru@gmail.com>
2022-09-06 14:33:30 -0300

    Implementation tab_layout e view_pager

--- app/src/main/java/com/example/myfood/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/myfood/ui/home/HomeFragment.kt
@@ -10,8 +10,13 @@ import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.myfood.R
 import com.example.myfood.databinding.FragmentHomeBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class HomeFragment : Fragment() {
 
@@ -32,6 +37,7 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        setupViews()
 
         homeViewModel.text.observe(viewLifecycleOwner) {
             Log.e("HOME", activity.toString())
@@ -39,8 +45,35 @@ class HomeFragment : Fragment() {
         return root
     }
 
+    private fun setupViews() {
+        val tab_layout = binding.addTab
+        val view_pager = binding.addViewpage
+        view_pager.isUserInputEnabled = true
+        val adapter = TabViewPageAdapter(this)
+        view_pager.adapter = adapter
+
+        TabLayoutMediator(tab_layout, view_pager) { tab, position ->
+            tab.text = getString(adapter.tabs[position])
+        }.attach()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+}
+
+class TabViewPageAdapter(fa: Fragment) : FragmentStateAdapter(fa){
+    val tabs = arrayOf(R.string.restaurants, R.string.marketplaces, R.string.drinks)
+    val fragments = arrayOf(MarketplaceFragment(), MarketplaceFragment(), MarketplaceFragment())
+
+    override fun getItemCount() = fragments.size
+
+    override fun createFragment(position: Int): Fragment {
+        return fragments[position]
+    }
+}
+
+class MarketplaceFragment: Fragment(){
+
 }
